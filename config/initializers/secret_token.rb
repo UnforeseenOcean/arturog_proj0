967d622eb4727959329795edf0fd41a0d00e989b
a-gonzalezii <a.gonzalezii609@gmail.com>
2013-09-15 18:13:03 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ArturogProj0::Application.config.secret_key_base = 'e43464d77b6102affd94daedf79eaf1cca50818eaed78eb7aeb8164f4765d21e0730f6511b1db08036e09776f06dd81b046fcbb851eb5e3bee1a76f8cc044bcf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ArturogProj0::Application.config.session_store :cookie_store, key: '_arturog_proj0_session'
